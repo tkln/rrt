af3fb4c0dd69bb17eef60131278c10d839a283c5
Aapo Vienamo <aapo.vienamo@iki.fi>
2020-12-20 23:19:21 +0200

    main: Implement PPM image save

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ use std::mem;
 mod vec3;
 use vec3::Vec3;
 
+fn save_image(w: usize, h: usize, pixels: &[Vec3]) {
+    println!("P3");
+    println!("{} {}", w, h);
+    println!("255");
+    for y in 0..h {
+        for x in 0..w {
+            let p = pixels[x + y * w] * 255.99;
+            print!("{} {} {} ", p.x as u32, p.y as u32, p.z as u32);
+        }
+        println!("");
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     println!("{}", mem::align_of::<Vec3>());

ba42336a36a215d0ce77e8ecea538f2981df92c5
Aapo Vienamo <aapo.vienamo@iki.fi>
2020-12-20 23:26:51 +0200

    main: Draw a gradient

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,21 @@ fn save_image(w: usize, h: usize, pixels: &[Vec3]) {
 }
 
 fn main() {
-    println!("Hello, world!");
-    println!("{}", mem::align_of::<Vec3>());
     let a = Vec3::new(1.0, 2.0, 3.0);
     let mut b = Vec3::one();
     b += a;
+
+    let w = 256;
+    let h = 256;
+    let mut img = vec![Vec3::zero(); w * h];
+
+    for x in 0..h {
+        for y in 0..w {
+            img[x + y * w] = Vec3::new((x as f32) / (w as f32),
+                                       (y as f32) / (h as f32),
+                                       0.25);
+        }
+    }
+
+    save_image(w, h, &img);
 }

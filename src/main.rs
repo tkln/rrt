c1be64ac9fc2b0a5aa7e55a39969a25b02b9df14
Aapo Vienamo <aapo.vienamo@iki.fi>
2020-12-23 12:46:20 +0200

    main: Factor camera code out

--- src/main.rs
+++ src/main.rs
@@ -32,21 +32,41 @@ fn get_color(ray: &Ray, hittables: &HittableList) -> Vec3 {
     Vec3::one() * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
 }
 
+struct Camera {
+    orig: Vec3,
+    lower_left: Vec3,
+    horiz: Vec3,
+    vert: Vec3,
+}
+
+impl Camera {
+    fn new(ar: f32) -> Camera {
+        let viewport_h = 2.0;
+        let viewport_w = ar * viewport_h;
+        let focal_len = 1.0;
+
+        let orig = Vec3::new(0.0, 0.0, 0.0);
+        let horiz = Vec3::new(viewport_w, 0.0, 0.0);
+        let vert = Vec3::new(0.0, viewport_h, 0.0);
+        let lower_left = orig - horiz * 0.5 - vert * 0.5 -
+                         Vec3::new(0.0, 0.0, focal_len);
+
+        Camera { orig: orig, lower_left: lower_left, horiz: horiz, vert: vert }
+    }
+
+    fn get_ray(&self, u: f32, v: f32) -> Ray {
+        let dir = self.lower_left + self.horiz * u + self.vert * v - self.orig;
+        Ray::new(self.orig, dir)
+    }
+}
+
 fn main() {
     let img_ar = 16.0 / 9.0;
     let img_w = 400;
     let img_h = (img_w as f32  / img_ar) as usize;
     let mut img = vec![Vec3::zero(); img_w * img_h];
 
-    let viewport_h = 2.0;
-    let viewport_w = img_ar * viewport_h;
-    let focal_len = 1.0;
-
-    let orig = Vec3::new(0.0, 0.0, 0.0);
-    let horiz = Vec3::new(viewport_w, 0.0, 0.0);
-    let vert = Vec3::new(0.0, viewport_h, 0.0);
-    let lower_left = orig - horiz * 0.5 - vert * 0.5 -
-                     Vec3::new(0.0, 0.0, focal_len);
+    let cam = Camera::new(img_ar);
 
     let hittables = HittableList {
         hittables: vec![
@@ -59,7 +79,7 @@ fn main() {
         for x in 0..img_w {
             let u = (x as f32) / ((img_w - 1) as f32);
             let v = ((img_h - y) as f32) / ((img_h - 1) as f32);
-            let ray = Ray::new(orig, lower_left + horiz * u + vert * v - orig);
+            let ray = cam.get_ray(u, v);
             img[x + y * img_w] = get_color(&ray, &hittables);
         }
     }

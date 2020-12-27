6d6bc73093d5829516faf604ddea83a0a0d6aa64
Aapo Vienamo <aapo.vienamo@iki.fi>
2020-12-27 18:52:09 +0200

    material: Add Dielectric

--- src/material.rs
+++ src/material.rs
@@ -53,4 +53,28 @@ impl Material for Metal {
     }
 }
 
+#[derive(Copy, Clone)]
+pub struct Dielectric {
+    ir: f32,
+}
 
+impl Dielectric {
+    pub fn new(ir: f32) -> Dielectric {
+        Dielectric{ir}
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(&self, ray_in: &Ray, rec: &HitRecord, rng: &mut RNG) -> Option<(Vec3, Ray)> {
+        let attenuation = Vec3::one();
+        let refraction_ratio = if rec.front_face {
+            1.0 / self.ir
+        } else {
+            self.ir
+        };
+        let unit_dir = ray_in.dir.normalized();
+        let refracted = Vec3::refract(unit_dir, rec.n, refraction_ratio);
+        let scattered = Ray::new(rec.p, refracted);
+        Some((attenuation, scattered))
+    }
+}

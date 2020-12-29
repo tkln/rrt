ed01391a4b8300ce8f9978bae33f791ab1ae9e5e
Aapo Vienamo <aapo.vienamo@iki.fi>
2020-12-29 19:36:41 +0200

    bvh: Implement BVH

--- src/bvh.rs
+++ src/bvh.rs
@@ -1,9 +1,23 @@
+use std::cmp::Ordering;
+use std::fmt;
+
 use crate::Vec3;
 use crate::Ray;
+use crate::hittable::{Hittable, HitRecord, HittableList};
+use crate::rng::RNG;
 
+#[derive(Copy, Clone)]
+enum Axis {
+    X = 0,
+    Y = 1,
+    Z = 2,
+}
+
+#[derive(Copy, Clone)]
+#[derive(Debug)]
 pub struct AABB {
-    min: Vec3,
-    max: Vec3,
+    pub min: Vec3,
+    pub max: Vec3,
 }
 
 impl AABB {
@@ -46,5 +60,132 @@ impl AABB {
                             box_a.max.y.max(box_b.max.y),
                             box_a.max.z.max(box_b.max.z));
         AABB::new(min, max)
+    }
+
+    fn get_longest_axis(&self) -> Axis {
+        let size = (self.max - self.min).abs();
+        if size.x > size.y && size.x > size.z {
+            Axis::X
+        } else if size.y > size.z {
+            Axis::Y
+        } else {
+            Axis::Z
+        }
+    }
+}
+
+pub enum BVH<'a> {
+    Node {
+        left: Box::<BVH<'a>>,
+        right: Box::<BVH<'a>>,
+        aabb: AABB,
+    },
+    Leaf {
+        hittable: &'a dyn Hittable,
+    }
+}
+
+impl BVH<'_> {
+    pub fn new<'a>(items: Vec<&'a dyn Hittable>) -> BVH<'a> {
+        let mut objs = items;
+        let span = objs.len();
+
+        if span == 1 {
+            return BVH::Leaf {
+                hittable: objs[0],
+            };
+        }
+
+        let aabb = AABB::union(&objs).unwrap();
+
+        let axis = aabb.get_longest_axis();
+        let cmp = |a: AABB, b: AABB| -> std::cmp::Ordering {
+            let idx = axis as usize;
+            let va = a.min[idx];
+            let vb = b.min[idx];
+            if va == vb {
+                Ordering::Equal
+            } else if va > vb {
+                Ordering::Greater
+            } else {
+                Ordering::Less
+            }
+        };
+
+        if span == 2 {
+            let a = Box::new(BVH::Leaf { hittable: objs[0] });
+            let b = Box::new(BVH::Leaf { hittable: objs[1] });
+            if cmp(objs[0].get_aabb().unwrap(),
+                   objs[1].get_aabb().unwrap()) == Ordering::Less {
+                return BVH::Node {
+                    left: a,
+                    right: b,
+                    aabb: aabb,
+                };
+            } else {
+                return BVH::Node {
+                    left: b,
+                    right: a,
+                    aabb: aabb,
+                };
+            }
+        } else {
+            objs.sort_by(|a, b| cmp(a.get_aabb().unwrap(),
+                                    b.get_aabb().unwrap()));
+            let partition = span / 2;
+
+            return BVH::Node {
+                left: Box::new(BVH::new(objs[..partition].to_vec())),
+                right: Box::new(BVH::new(objs[partition..].to_vec())),
+                aabb: aabb,
+            };
+        }
+    }
+}
+
+impl Hittable for BVH<'_> {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, rng: &mut RNG) -> Option<HitRecord> {
+        match self {
+            BVH::Node { left, right, aabb } =>
+            {
+                if !aabb.hit(ray, t_min, t_max) {
+                    return None
+                }
+
+                let hit_left = left.hit(ray, t_min, t_max, rng);
+                let hit_right = right.hit(ray, t_min, t_max, rng);
+
+                match (hit_left, hit_right) {
+                    (Some(l), Some(r))  => Some(if l.t < r.t { l } else { r }),
+                    (None, Some(r))     => Some(r),
+                    (Some(l), None)     => Some(l),
+                    (None, None)        => None,
+                }
+            }
+
+            BVH::Leaf { hittable } => hittable.hit(ray, t_min, t_max, rng),
+        }
+    }
+
+    fn get_aabb(&self) -> Option<AABB> {
+        match self {
+            BVH::Node { left: _, right: _, aabb } => Some(*aabb),
+            BVH::Leaf { hittable } => hittable.get_aabb(),
+        }
+    }
+}
+
+impl fmt::Debug for BVH<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &*self {
+            BVH::Node { left, right, aabb }=>
+            f.debug_struct("Node")
+             .field("left", &left)
+             .field("right", &right)
+             .field("aabb", &aabb)
+             .finish(),
+            BVH::Leaf {hittable: _} =>
+            f.debug_struct("Leaf").finish(),
+        }
     }
 }
